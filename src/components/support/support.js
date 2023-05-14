da4d35d462fde7724bccdf7a2610e34526215f2e
dewansh07 <dewanshwadhwa@gmail.com>
2023-05-15 01:25:49 +0530

    added form fields

--- src/components/support/support.js
+++ src/components/support/support.js
@@ -1,70 +1,96 @@
+import React, { useState } from 'react';
+import './support.css';
 
-import { Navbar } from '../../Navbar/Navbar';
-import React, { useState } from "react";
-import "./support.css";
 function Support() {
+  const [productDetails, setProductDetails] = useState('');
+  const [errorDescription, setErrorDescription] = useState('');
+  const [reproductionSteps, setReproductionSteps] = useState('');
+  const [deviceInfo, setDeviceInfo] = useState('');
+  const [attachment, setAttachment] = useState(null);
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
 
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    issue: "",
-  });
-
-  function handleInputChange(event) {
-    const { name, value } = event.target;
-    setFormData({ ...formData, [name]: value });
-  }
-
-  function handleSubmit(event) {
+  const handleSubmit = (event) => {
     event.preventDefault();
-    fetch("/api/support", {
-      method: "POST",
-      body: JSON.stringify(formData),
-    })
-      .then((response) => response.json())
-      .then((data) => console.log(data))
-      .catch((error) => console.error(error));
-  }
+    // handle form submission
+  };
 
   return (
-
-    <div className="form-container">
-      <Navbar />
-      <label className="form-label" htmlFor="name">
-        Name:
-      </label>
-      <input
-        className="form-input"
-        type="text"
-        id="name"
-        name="name"
-        value={formData.name}
-        onChange={handleInputChange}
-      />
-      <label className="form-label" htmlFor="email">
-        Email:
-      </label>
-      <input
-        className="form-input"
-        type="email"
-        id="email"
-        name="email"
-        value={formData.email}
-        onChange={handleInputChange}
-      />
-      <label className="form-label" htmlFor="issue">
-        Describe the issue:
-      </label>
-      <textarea
-        className="form-textarea"
-        id="issue"
-        name="issue"
-        value={formData.issue}
-        onChange={handleInputChange}
-      ></textarea>
-      <button className="form-submit" type="submit">
-        Submit
-      </button>
+    <div>
+      <h2>Contact Support</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Product Details:
+          <input
+            type="text"
+            value={productDetails}
+            onChange={(event) => setProductDetails(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Error Description:
+          <textarea
+            value={errorDescription}
+            onChange={(event) => setErrorDescription(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Reproduction Steps:
+          <textarea
+            value={reproductionSteps}
+            onChange={(event) => setReproductionSteps(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Device Information:
+          <input
+            type="text"
+            value={deviceInfo}
+            onChange={(event) => setDeviceInfo(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Attachment:
+          <input
+            type="file"
+            onChange={(event) => setAttachment(event.target.files[0])}
+          />
+        </label>
+        <br />
+        <label>
+          Name:
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Phone:
+          <input
+            type="tel"
+            value={phone}
+            onChange={(event) => setPhone(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
